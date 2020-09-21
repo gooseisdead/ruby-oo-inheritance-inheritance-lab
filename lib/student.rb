7fd33f39f6338bd3333993b112bea054784f6daf
gooseisdead <gooseisdead@mac.com>
2020-09-21 00:55:08 -0400

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "../lib/user.rb"
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(kernel_of_info)
+        @knowledge << kernel_of_info
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
